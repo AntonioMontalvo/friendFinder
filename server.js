a59e5ec1fa3cb132288256bfd39d4681c805a3d4
Antonio Montalvo <montalvocode@yahoo.com>
2016-09-28 16:23:49 -0700

    modify PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require('path');
 var app = express();//assign express to app
 
 //When hosting the application on another enviroment like Heroku, your host may confifure the process.env.PORT for you, So the solution is setting PORT to be whatever the enviroment PORT is on Hreroku, or 3000 if you are not on Heroku, like your local machine.
-var PORT = process.env.PORT || 3000;
+var PORT = (process.env.PORT || 3000);
 
 //Sets up how express uses 'body-parser'/ Now the server can intrepret data sent to it.
 
