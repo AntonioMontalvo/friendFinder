f55025e534b8969f09acc3a2787ac96c80b5371f
Antonio Montalvo <montalvocode@yahoo.com>
2016-09-29 18:11:44 -0700

    correct algo

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -27,70 +27,57 @@ module.exports = function (app){
 
 	app.post('/api/friends', function(req, res){
 		userArray.push(req.body.scores);
-		
 
+		function findDifferences (user, match){
+		  for (var i = 0; i < friendsData.length; i++){
+		    for (var j = 0; j < 10; j++){
+		      totalDifference += Math.abs(user[userArray.length-1][j] - parseInt(match[i].scores[j]));
+		    }
+		    friendsData[i].difference += totalDifference;
+		    // console.log(totalDifference)
+		    // console.log(friendsData[i].name + ' has a difference of '
+		    //           +friendsData[i].difference + ' with you!');
+		    totalDifference = 0;
+		  }
+		  
+		}
+		findDifferences(userArray, friendsData);
 
-function findDifferences (user, match){
-  for (var i = 0; i < friendsData.length; i++){
-    for (var j = 0; j < 10; j++){
-      totalDifference += Math.abs(user[0][j] - parseInt(match[i].scores[j]));
-    }
-    friendsData[i].difference += totalDifference;
-    // console.log(totalDifference)
-    // console.log(friendsData[i].name + ' has a difference of '
-    //           +friendsData[i].difference + ' with you!');
-    totalDifference = 0;
-  }
-  
-}
-findDifferences(userArray, friendsData);
-
-
- function getAmigo(friends) {
-   
-  var count = 0;
-      for(var i = 0; i < friendsData.length; i++){
-        for (var j = 0; j < 10; j++){
-          
-          if(friendsData[i].difference === 0){
-          	superFriend = friendsData[i];
-            // console.log('You found your match in ' + friendsData[i].name );
-          return;
-          }
+ 		function getAmigo(friends) {   
+			var count = 0;
+      		for(var i = 0; i < friendsData.length; i++){
+        		for (var j = 0; j < 10; j++){
+          			if(friendsData[i].difference === 0){
+          				superFriend = friendsData[i];
+            			// console.log('You found your match in ' + friendsData[i].name );
+          				return;
+      				}
           
           
-          if(friendsData[i].difference < friendsData[j].difference){
-            count++;
-//             
-            // console.log(friendsData[i].name + ' difference is ' + friendsData[i].difference + 
-            //             ' and is smaller than  ' + friendsData[j].name +
-            //             " whose difference value is " + friendsData[j].difference );
-            
-          }
-          if(count === 9){
-          console.log('You found your match in ' + friendsData[i].name );
-          superFriend = friendsData[i];
-          return;
-          }
-          if (count !==9){
-          	superFriend = friendsData[0];
-          }
+	          		if(friendsData[i].difference < friendsData[j].difference){
+	            		count++;             
+				        // console.log(friendsData[i].name + ' difference is ' + friendsData[i].difference + 
+				        //             ' and is smaller than  ' + friendsData[j].name +
+				        //             " whose difference value is " + friendsData[j].difference );
+	            
+	          		}
+	          		if(count === 9){
+	          			console.log('You found your match in ' + friendsData[i].name );
+	          			superFriend = friendsData[i];
+	          			return;
+	          		}
+	          		if (count !==9){
+	          			superFriend = friendsData[0];
+	      			}
           
           
-        }
-        // console.log('I COUNT ' + count + ' TIMES');
-        count = 0;
-    }
- }
- 
-getAmigo(friendsData);
+    			}
+        		// console.log('I COUNT ' + count + ' TIMES');
+  		    	count = 0;
+    		}
+ 		}
+		getAmigo(friendsData);
 
-
-res.send(superFriend);
-		
-
-
+		res.send(superFriend);
 	});
-
-
 };
